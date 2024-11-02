8470d4ab241a97c5975cfd4a336ce9445396c7d0
rmnyirenda <bsc-com-29-21@unima.ac.mw>
2024-11-02 11:37:55 +0200

    installed tailwind css, changed layout.tsx file

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,28 +1,10 @@
 import { Image, StyleSheet, Platform ,View,Text} from 'react-native';
 
-export default function HomeScreen() {
+export default function Login() {
   return (
-<View style={styles.titleContainer}>
+<View >
   <Text>Hello</Text>
 </View>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
