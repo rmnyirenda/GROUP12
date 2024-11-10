8e51ab614dd3946fc40cf209892c7db3bc3883d8
rmnyirenda <bsc-com-29-21@unima.ac.mw>
2024-11-10 20:20:42 +0200

    Deleted files

---
+++ app/StudentDashboard/Assigned.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Assigned() {
+  return (
+    <View>
+      <Text>Assigned Screen</Text>
+    </View>
+  );
+}
---
+++ app/StudentDashboard/Missed.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Missed() {
+  return (
+    <View>
+      <Text>Missed Screen</Text>
+    </View>
+  );
+}
