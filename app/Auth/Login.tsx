238bc8e40fcbbb3e884fd6e83199c892bd6169c5
YOHANE KUMWENDA <yohanekumwendah@gmail.com>
2024-11-25 23:08:16 -1200

    changing styling to updateuser component

--- app/Auth/Login.tsx
+++ app/Auth/Login.tsx
@@ -1,86 +1,95 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Image, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
-import {  FontAwesome } from '@expo/vector-icons';
-import {Firebase_Auth} from '../../firebaseConfig'
-import { signInWithEmailAndPassword } from 'firebase/auth';
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  Image,
+  KeyboardAvoidingView,
+  ActivityIndicator,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Firebase_Auth } from "../../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = ({ navigation }: any) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const[loading,setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const auth = Firebase_Auth;
 
-
-  const signIn =async () => {
+  const signIn = async () => {
     setLoading(true);
-    try{
+    try {
       const response = await signInWithEmailAndPassword(auth, email, password);
       console.log(response);
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
-    }finally{
+    } finally {
       setLoading(false);
     }
-   
-       if (email === 'letcom311@gmail.com' && password === 'le7654321'||
-        email === 'letcom312@gmail.com' && password === 'let12345' ||
-        email === 'letcom313@gmail.com' && password === 'le123456' ||
-        email === 'letcom314@gmail.com' && password === 'le1234567' ||
-        email === 'letcom315@gmail.com' && password === 'le123456789'
-       ) {
-        navigation.navigate('LecturerDashboard');
-      }
-    else {
-      Alert.alert('Error', 'Invalid credentials. Please try again.');
+
+    if (
+      (email === "letcom311@gmail.com" && password === "le7654321") ||
+      (email === "letcom312@gmail.com" && password === "let12345") ||
+      (email === "letcom313@gmail.com" && password === "le123456") ||
+      (email === "letcom314@gmail.com" && password === "le1234567") ||
+      (email === "letcom315@gmail.com" && password === "le123456789")
+    ) {
+      navigation.navigate("LecturerDashboard");
+    } else {
+      Alert.alert("Error", "Invalid credentials. Please try again.");
     }
-  }
- 
+  };
+
   return (
     <View style={styles.container}>
-      <KeyboardAvoidingView >
-      {/* Logo */}
-      <Image 
-        source={{ uri: 'https://png.pngtree.com/png-clipart/20211017/original/pngtree-school-logo-png-image_6851480.png' }}
-        style={styles.logo}
-      />
-
-      {/* Header */}
-      <Text style={styles.header}>SIGN IN</Text>
-
-      {/* Username Input */}
-      <View style={styles.inputContainer}>
-        <FontAwesome name="user" size={20} color="#000" />
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
+      <KeyboardAvoidingView>
+        {/* Logo */}
+        <Image
+          source={{
+            uri: "https://png.pngtree.com/png-clipart/20211017/original/pngtree-school-logo-png-image_6851480.png",
+          }}
+          style={styles.logo}
         />
-      </View>
 
-      {/* Password Input */}
-      <View style={styles.inputContainer}>
-        <FontAwesome name="key" size={20} color="#000" />
-        <TextInput
-          style={styles.input}
-          placeholder="PASSWORD"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={setPassword}
-        />
-      </View>
+        {/* Header */}
+        <Text style={styles.header}>SIGN IN</Text>
 
-      {/* Login Button */}
-        {loading? ( 
-          <ActivityIndicator color='red'/>
-        ):(
-      <TouchableOpacity style={styles.button} onPress={signIn}>
-        <Text style={styles.buttonText}>LOG IN</Text>
-      </TouchableOpacity>
+        {/* Username Input */}
+        <View style={styles.inputContainer}>
+          <FontAwesome name="user" size={20} color="#000" />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+
+        {/* Password Input */}
+        <View style={styles.inputContainer}>
+          <FontAwesome name="key" size={20} color="#000" />
+          <TextInput
+            style={styles.input}
+            placeholder="PASSWORD"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+
+        {/* Login Button */}
+        {loading ? (
+          <ActivityIndicator color="red" />
+        ) : (
+          <TouchableOpacity style={styles.button} onPress={signIn}>
+            <Text style={styles.buttonText}>LOG IN</Text>
+          </TouchableOpacity>
         )}
-      <TouchableOpacity>Reset password?</TouchableOpacity>
+        <TouchableOpacity>Reset password?</TouchableOpacity>
       </KeyboardAvoidingView>
     </View>
   );
@@ -89,9 +98,9 @@ const Login = ({ navigation }: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f9f9f9',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f9f9f9",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   logo: {
@@ -101,20 +110,20 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#1c1cf0',
+    fontWeight: "bold",
+    color: "#1c1cf0",
     marginBottom: 20,
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: '#1c1cf0',
+    borderColor: "#1c1cf0",
     borderRadius: 8,
     paddingHorizontal: 10,
     paddingVertical: 5,
     marginBottom: 15,
-    width: '100%',
+    width: "100%",
   },
   input: {
     flex: 1,
@@ -122,18 +131,18 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
   },
   pickerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: '#1c1cf0',
+    borderColor: "#1c1cf0",
     borderRadius: 8,
     paddingHorizontal: 10,
     marginBottom: 15,
-    width: '100%',
+    width: "100%",
   },
   pickerLabel: {
     fontSize: 16,
-    color: '#333',
+    color: "#333",
     marginRight: 10,
   },
   picker: {
@@ -141,17 +150,17 @@ const styles = StyleSheet.create({
     height: 40,
   },
   button: {
-    backgroundColor: '#1c1cf0',
+    backgroundColor: "#1c1cf0",
     paddingVertical: 15,
     borderRadius: 8,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
     marginTop: 10,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
